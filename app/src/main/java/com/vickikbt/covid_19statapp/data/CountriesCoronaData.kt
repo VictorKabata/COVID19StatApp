c7ff5b64695a88745da874046fd0cf6dc9c77ec8
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-03-30 20:51:45 +0300

    Changing the CountriesFragment layout

---
+++ app/src/main/java/com/vickikbt/covid_19statapp/data/CountriesCoronaData.kt
@@ -0,0 +1,6 @@
+package com.vickikbt.covid_19statapp.data
+
+
+import com.google.gson.annotations.SerializedName
+
+class CountriesCoronaData : ArrayList<CountriesCoronaDataItem>()
---
+++ app/src/main/java/com/vickikbt/covid_19statapp/data/CountryInfo.kt
@@ -0,0 +1,14 @@
+package com.vickikbt.covid_19statapp.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CountryInfo(
+    val flag: String,
+    @SerializedName("_id")
+    val id: Int,
+    val iso2: String,
+    val iso3: String,
+    val lat: Int,
+    val long: Int
+)
