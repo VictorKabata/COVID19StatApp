4b13462e500ff9c30927faab0720a17c81230605
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-03-30 16:29:19 +0300

    Not responding

--- app/src/main/java/com/vickikbt/covid_19statapp/ui/global/GlobalFragment.kt
+++ app/src/main/java/com/vickikbt/covid_19statapp/ui/global/GlobalFragment.kt
@@ -5,30 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.vickikbt.covid_19statapp.R
 import com.vickikbt.covid_19statapp.databinding.FragmentGlobalBinding
-import kotlinx.android.synthetic.main.fragment_global.*
-import org.kodein.di.Kodein
+import com.vickikbt.covid_19statapp.util.ScopedFragment
+import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class GlobalFragment : Fragment(), KodeinAware {
+class GlobalFragment : ScopedFragment(), KodeinAware {
 
     override val kodein by closestKodein()
 
     lateinit var binding: FragmentGlobalBinding
     private lateinit var viewModel: GlobalViewModel
+    private val viewModelFactory: GlobalViewModelFactory by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(GlobalViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_global, container, false)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(GlobalViewModel::class.java)
 
 
         return binding.root
@@ -37,10 +38,17 @@ class GlobalFragment : Fragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        viewModel.cases.observe(viewLifecycleOwner, Observer {
-            text_home.text = it
-        })
+        bindUI()
 
     }
+
+    private fun bindUI() = launch {
+        val currentGlobalStat = viewModel.globalStatistics.await()
+        currentGlobalStat.observe(viewLifecycleOwner, Observer {
+            if(it==null)return@Observer
+
+            binding.textHome.text = it.toString()
+        })
+    }.cancel()
 
 }
--- app/src/main/java/com/vickikbt/covid_19statapp/ui/global/GlobalViewModelFactory.kt
+++ app/src/main/java/com/vickikbt/covid_19statapp/ui/global/GlobalViewModelFactory.kt
@@ -1,7 +1,14 @@
 package com.vickikbt.covid_19statapp.ui.global
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.vickikbt.covid_19statapp.db.repository.CoronaRepository
 
-class GlobalViewModelFactory (){
+@Suppress("UNCHECKED_CAST")
+class GlobalViewModelFactory(private val coronaRepository: CoronaRepository) :
+    ViewModelProvider.NewInstanceFactory() {
 
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return GlobalViewModel(coronaRepository) as T
+    }
 }
